33a970a8031b8966c1acc534e0b29262a77b0d34
saran1923 <24becse178@kahedu.edu.in>
2026-05-10 16:57:11 +0530

    Recover codebase and apply Vercel fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import typography from '@tailwindcss/typography';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -25,5 +27,5 @@ export default {
       },
     },
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [typography],
 }
