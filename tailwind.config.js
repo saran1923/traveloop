d637478d4e13a9710d8cdde7bba6b893c33ef773
OCTOBER-sk <sk6384184459@gmail.com>
2026-05-10 10:02:21 +0530

    Setup dependencies and config as per master plan

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // We can add custom colors here based on the design studio vibe mentioned in the plan
+      },
+    },
+  },
+  plugins: [],
+}
